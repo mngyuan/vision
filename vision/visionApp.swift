05f55dfec1deb3cdea228caa6c1e8b16ff4b3aaf
Kevin Lee <me@mngyuan.com>
2021-02-15 20:12:59 +0000

    Initial Commit

---
+++ vision/visionApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionApp.swift
+//  vision
+//
+//  Created by Kevin Lee on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
